fb337f3f8d589f74ad4f47157a24779d25794a35
lucasdonivieira <lucasdonivieira@gmail.com>
2020-12-18 15:32:05 -0300

    Update script.php

--- script.php
+++ script.php
@@ -1,9 +1,10 @@
 <?php
+session_start();
 require_once("functions.php");
 $conn = conectaBanco();
 
 if(!empty($_POST)) {
-    $sql= 'INSERT INTO `pontuacao`(`id_usuario`, `pontuacao`, `nivel`, `tempo`) VALUES (1,'.$_POST['pontuacao'].','.$_POST['nivel'].',\''.$_POST['timer'].'\')';
+    $sql= 'INSERT INTO `pontuacao`(`id_usuario`, `pontuacao`, `nivel`, `tempo`) VALUES ('.$_SESSION['idUser'].','.$_POST['pontuacao'].','.$_POST['nivel'].',\''.$_POST['timer'].'\')';
     $conn->exec($sql);
 }
 ?>
